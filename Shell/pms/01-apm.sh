fc27b67cd51d48574cca6c0a72e2d9d5e4d41672
fusion809 <brentonhorne77@gmail.com>
2016-08-21 20:49:23 +1000

    Updating scripts

--- Shell/pms/01-apm.sh
+++ Shell/pms/01-apm.sh
@@ -8,6 +8,16 @@ function apmr {
   apm remove "$@"
 }
 
+# search apm packages
+function apms {
+  apm search "$@"
+}
+
+# list installed apm packages
+function apml {
+  apm list
+}
+
 # Update Atom packages
 function apmup {
   apm update --no-confirm
