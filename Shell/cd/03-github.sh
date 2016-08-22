2cd4304d2bed359c09cb8bc687f5fcb452b535ef
fusion809 <brentonhorne77@gmail.com>
2016-08-22 16:05:31 +1000

    Updating scripts

--- Shell/cd/03-github.sh
+++ Shell/cd/03-github.sh
@@ -1,38 +1,38 @@
 # GitHub
 function cdg {
-	cd $GHUB/$1
+	cd "$GHUB/$1"
 }
 
 function cdgm {
-	cd $GHUBM/$1
+	cd "$GHUBM/$1"
 }
 
 function cdga {
-	cdgm atom/$1
+	cdgm "atom/$1"
 }
 
 function cdgo {
-	cd $GHUBO/$1
+	cd "$GHUBO/$1"
 }
 
 function cdai {
-	cdgm atom-installer/$1
+	cdgm "atom-installer/$1"
 }
 
 function cdds {
-	cdgm debian-scripts/$1
+	cdgm "debian-scripts/$1"
 }
 
 alias cdss=cdds
 
 function cdfgi {
-	cdgm fusion809.github.io/$1
+	cdgm "fusion809.github.io/$1"
 }
 
 function cdvsi {
-	cdgm vscode-installer/$1
+	cdgm "vscode-installer/$1"
 }
 
 function cdpk {
-	cd $PKG/$1
+	cd "$PKG/$1"
 }
