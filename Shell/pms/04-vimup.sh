ae8cfa4a3f4d8e10c3a93536405beaabfd138ff1
fusion809 <brentonhorne77@gmail.com>
2016-09-27 05:30:50 +1000

    Updating scripts

---
+++ Shell/pms/04-vimup.sh
@@ -0,0 +1,15 @@
+function vimup {
+  if [[ -d $GHUBO/vim ]]; then
+    git clone https://github.com/vim/vim $GHUBO/vim
+  fi
+  cdgo vim
+  git stash
+  git pull --all
+  git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+  unset srcdir
+  ./configure --prefix
+  make
+  sudo checkinstall
+  mv *.deb $HOME/Programs
+  cd ..
+}
