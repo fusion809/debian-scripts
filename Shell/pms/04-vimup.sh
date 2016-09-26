f7078c735150be0ebd2a21a54651323fcdb705b8
fusion809 <brentonhorne77@gmail.com>
2016-09-27 07:05:01 +1000

    Updating scripts

--- Shell/pms/04-vimup.sh
+++ Shell/pms/04-vimup.sh
@@ -2,15 +2,19 @@ function vimup {
   if ! [[ -d $GHUBO/vim ]]; then
     git clone https://github.com/vim/vim $GHUBO/vim
   fi
+  PWD=$(pwd)
   cdgo vim
   git stash
   make distclean
   git pull --all
-  git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+  pkgver=$(git describe --tags `git rev-list --tags --max-count=1`)
   unset srcdir
-  ./configure --prefix
+  cdp
+  curl -L https://github.com/vim/vim/archive/$pkgver.tar.gz | tar xz --transform="s/vim-${pkgver//v/}/vim/"
+  cd vim
+  ./configure --prefix=/usr
   make
   sudo checkinstall
   mv *.deb $HOME/Programs/Deb
-  cd -
+  cd $PWD
 }
