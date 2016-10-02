6d437db4998bc0ab115bd8bf1c35129163bc48e8
fusion809 <brentonhorne77@gmail.com>
2016-10-03 00:58:16 +1000

    Updating scripts

---
+++ Shell/edit/04-vscode.sh
@@ -0,0 +1,17 @@
+function vsfgi {
+	code-oss $FGI
+}
+
+alias vsblog=vsfgi
+
+function vssh {
+	code-oss $SHL
+}
+
+function vspy {
+	code-oss $PY
+}
+
+function vssp {
+	code-oss $HOME/Documents/CodeLite/CPP-Math-Projects/Simple-Pendulum
+}
--- Shell/variables/02-github.sh
+++ Shell/variables/02-github.sh
@@ -4,3 +4,5 @@ export GHUBM=$GHUB/mine
 export GHUBO=$GHUB/others
 export FGI=$GHUBM/fusion809.github.io
 export DBS=$GHUBM/debian-scripts
+export PY=$GHUBM/python-scripts
+export SHL=$HOME/Shell
