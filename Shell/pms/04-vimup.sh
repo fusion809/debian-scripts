e815bfd1e0cfa2441bf1b104a832775953d384b3
fusion809 <brentonhorne77@gmail.com>
2016-10-16 01:25:02 +1000

    Updating vimup script to include GUI

--- Shell/pms/04-vimup.sh
+++ Shell/pms/04-vimup.sh
@@ -17,7 +17,7 @@ function vimup {
   make
   sudo checkinstall --maintainer="Brenton Horne \<brentonhorne77@gmail.com\>" --pkgversion="${_pkgver}"
   if ! [[ -f $HOME/Programs/Deb/vim*.deb ]]; then
-    rm $HOME/Programs/Deb/vim*.deb
+    sudo rm $HOME/Programs/Deb/vim*.deb
   fi
   mv vim_${_pkgver}-1_amd64.deb $HOME/Programs/Deb
   cd $PWD
