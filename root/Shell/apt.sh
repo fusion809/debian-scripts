059a81b4c9230501b80b9e59a4856b523c2f2ff7
fusion809 <brentonhorne77@gmail.com>
2016-10-16 10:00:07 +1000

    Updating vimup script to include GUI

--- root/Shell/apt.sh
+++ root/Shell/apt.sh
@@ -11,11 +11,11 @@ function apgi {
 }
 
 function update {
-	apt-get update && apt-get upgrade -y --allow-unauthenticated && apt-get autoremove -y && apt-get autoclean && su - fusion809 -c "apm update"
+	apt-get update && apt-get upgrade -y --allow-unauthenticated && apt-get autoremove -y && apt-get autoclean && su - fusion809 -c "apm update --no-confirm"
 }
 
 function updatef {
-	apt-fast update && apt-fast upgrade -y --allow-unauthenticated && apt-fast autoremove -y && apt-fast autoclean && su - fusion809 -c "apm update"
+	apt-fast update && apt-fast upgrade -y --allow-unauthenticated && apt-fast autoremove -y && apt-fast autoclean && su - fusion809 -c "apm update --no-confirm"
 }
 
 function afi {
