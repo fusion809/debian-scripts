69318c274aa0fb9cee07cc4c7a65f25e8b372bcb
fusion809 <brentonhorne77@gmail.com>
2016-11-05 12:31:49 +1000

    Updating scripts

---
+++ Shell/edit/atom/github/themes.sh
@@ -0,0 +1,16 @@
+# Themes
+function edt {
+  atom "$GHUBM/themes/$1"
+}
+
+function edmr {
+  edt "MacBuntu-Remix/$1"
+}
+
+function edoaw {
+  edt "OSX-Arc-White/$1"
+}
+
+function edsfr {
+  edt "Super-Flat-Remix/$1"
+}
