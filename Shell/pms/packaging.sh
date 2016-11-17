512444c4b02e9020b059f554fdfa5a0883b95cd2
fusion809 <brentonhorne77@gmail.com>
2016-11-18 00:34:04 +1000

    Updating scripts

---
+++ Shell/pms/packaging.sh
@@ -0,0 +1,14 @@
+function dbuild {
+  cdp octave
+  rm *xenial*
+  rm *trusty*
+  if ! [[ -d octave-4.2.0/debian ]]; then
+    mkdir -p octave-4.2.0/debian
+  fi
+  cp -a octave-debian/* octave-4.2.0/debian
+  cd octave-4.2.0
+  debuild -S -sa
+  cd ..
+  debsign -kk EB93141B *.changes
+  dput -f ppa:brentonhorne/octave *.changes
+}
