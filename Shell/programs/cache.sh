3e5dda3a6e98b219adaeaaf8bfc377425f2c39d7
fusion809 <brentonhorne77@gmail.com>
2016-11-21 00:38:44 +1000

    Updating scripts

--- Shell/programs/cache.sh
+++ Shell/programs/cache.sh
@@ -3,5 +3,5 @@ function upcac {
 }
 
 function maccp {
-  sudo cp /usr/share/icons/Numix-Circle/48/apps/$1.svg /usr/share/icons/MacBuntu-OS/apps/128 && sudo rm /usr/share/icons/MacBuntu-OS/48/apps/$1.png
+  sudo cp /usr/share/icons/Numix-Circle/48/apps/$1.svg /usr/share/icons/MacBuntu-OS/apps/128 && sudo rm /usr/share/icons/MacBuntu-OS/*/apps/$1.png
 }
