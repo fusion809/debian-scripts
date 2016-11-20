d57157948e9cdcac3d2e63403e3a24c28d0ad310
fusion809 <brentonhorne77@gmail.com>
2016-11-21 00:40:15 +1000

    Updating scripts

--- Shell/programs/cache.sh
+++ Shell/programs/cache.sh
@@ -5,3 +5,7 @@ function upcac {
 function maccp {
   sudo cp /usr/share/icons/Numix-Circle/48/apps/$1.svg /usr/share/icons/MacBuntu-OS/apps/128 && sudo rm /usr/share/icons/MacBuntu-OS/*/apps/$1.png
 }
+
+function upmac {
+  sudo update-icon-caches /usr/share/icons/MacBuntu-OS
+}
