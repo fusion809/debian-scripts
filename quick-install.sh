169fa182da2b679b311137f9e994699985aca163
fusion809 <brentonhorne77@gmail.com>
2016-11-23 20:51:26 +1000

    mkdir->mkdir -p

--- quick-install.sh
+++ quick-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Create GitHub directory
 if ! [[ -d $HOME/GitHub/mine/scripts ]]; then
-  mkdir $HOME/GitHub/mine/scripts
+  mkdir -p $HOME/GitHub/mine/scripts
 fi
 
 # Get openssh, if not pre-installed and Zsh
