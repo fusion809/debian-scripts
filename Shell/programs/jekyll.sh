5cc34fa346982be08bfe478c30e138996914eb49
Brenton Horne <brentonhorne77@gmail.com>
2016-12-20 03:11:07 +1000

    Updating

--- Shell/programs/jekyll.sh
+++ Shell/programs/jekyll.sh
@@ -1,19 +1,24 @@
-if ! `which jekyll > /dev/null 2>&1`; then
-  printf "Defining the jekyll command...\n"
-  if ! `which bundle exec jekyll > /dev/null 2>&1`; then
-    if ! `which bundle > /dev/null 2>&1`; then
-      if ! `which gem > /dev/null 2>&1`; then
-        sudo apt-get -f install gem
+# Install Jekyll and other rubygems to $FGI
+
+if [[ -d $FGI ]]; then
+  if ! `which jekyll > /dev/null 2>&1`; then
+    printf "Defining the jekyll command...\n"
+    if ! `which bundle exec jekyll > /dev/null 2>&1`; then
+      if ! `which bundle > /dev/null 2>&1`; then
+        if ! `which gem > /dev/null 2>&1`; then
+          sudo apt-get install -y gem ruby-dev
+        fi
+        sudo gem install bundler
       fi
-      sudo gem install bundler
+      pushd $FGI
+      bundle install
+      bundle update
+      popd
     fi
-    pushd $FGI
-    bundle install
-    bundle update
-    popd
   fi
 fi
 
+# Create binary script
 cat > /tmp/jekyll <<\EOF
 #!/bin/bash
 pushd /home/fusion809/GitHub/mine/websites/fusion809.github.io
--- Shell/variables/02-github.sh
+++ Shell/variables/02-github.sh
@@ -1,14 +1,62 @@
 # GHUB variables
 export GHUB=$HOME/GitHub
 export GHUBM=$GHUB/mine
+
+# GHUBO
 export GHUBO=$GHUB/others
-export ED=$GHUBM/editors
-export INS=$GHUBM/installers
-export PKG=$GHUBM/packaging
+if ! [[ -d $GHUBO ]]; then
+  mkdir -p $GHUBO
+fi
+
+# EDT
+export EDT=$GHUBM/editors
+if ! [[ -d $EDT ]]; then
+  mkdir -p $EDT
+fi
+
+# SCR
 export SCR=$GHUBM/scripts
-export THM=$GHUBM/themes
+if ! [[ -d $SCR ]]; then
+  mkdir -p $SCR
+fi
+
+# PKG
+export PKG=$GHUBM/packaging
+if ! [[ -d $PKG ]]; then
+  mkdir -p $PKG
+fi
+
+# WEB
 export WEB=$GHUBM/websites
+if ! [[ -d $WEB ]]; then
+  mkdir -p $WEB
+fi
+
+# ATO
+export ATO=$GHUBM/atom
+if ! [[ -d $ATO ]]; then
+  mkdir -p $ATO
+fi
+
+# ATO
+export INS=$GHUBM/installers
+if ! [[ -d $INS ]]; then
+  mkdir -p $INS
+fi
+
+# FGI
 export FGI=$WEB/fusion809.github.io
+if ! [[ -d $FGI ]]; then
+  if ! [[ -f $HOME/.fgilock ]]; then
+    printf "The Hornery was not locally detected on this system; do you want to clone it to $FGI? [y/n]"
+    read -p $fgilock
+  fi
+  if [[ $fgilock == "y" ]]; then
+    git clone https://github.com/fusion809/fusion809.github.io $FGI
+  else
+    echo $fgilock > $HOME/.fgilock
+  fi
+fi
 export DBS=$SCR/debian-scripts
 export PY=$SCR/python-scripts
 export SHL=$HOME/Shell
