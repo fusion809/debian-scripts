d289a66312165e4e78623348008c993ec0ffb62b
Brenton Horne <brentonhorne77@gmail.com>
2016-12-20 03:24:13 +1000

    Adjusting quick install

--- quick-install.sh
+++ quick-install.sh
@@ -1,61 +1,64 @@
 #!/bin/bash
+
+# SCR
+SCR="$HOME/GitHub/mine/scripts"
+DS="$SCR/debian-scripts"
+OZ="$HOME/.oh-my-zsh"
+ZT="$SCR/zsh-theme"
+
+FUS_URL="https://github.com/fusion809"
+ZSH_URL="https://github.com/zsh-users"
+
 # Create GitHub directory
-if ! [[ -d $HOME/GitHub/mine/scripts ]]; then
-  mkdir -p $HOME/GitHub/mine/scripts
+if ! [[ -d $SCR ]]; then
+  mkdir -p $SCR
 fi
 
 # Get openssh, if not pre-installed and Zsh
 sudo apt-get install -y openssh-server zsh git
 
 # Clone debian-scripts repo
-if ! [[ -d $HOME/GitHub/mine/scripts/debian-scripts ]]; then
-  git clone https://github.com/fusion809/debian-scripts $HOME/GitHub/mine/scripts/debian-scripts
+if ! [[ -d $DS ]]; then
+  git clone $FUS_URL/debian-scripts $DS
   # Copy across
-  cp -a $HOME/GitHub/mine/scripts/debian-scripts/{Shell,.bashrc,.zshrc} $HOME/
-  sudo cp -a $HOME/GitHub/mine/scripts/debian-scripts/root/{Shell,.bashrc,.zshrc} /root/
-elif [[ -d $HOME/GitHub/debian-scripts ]]; then
-  cd $HOME/GitHub/debian-scripts
+  cp -a $DS/{Shell,.bashrc,.zshrc} $HOME/
+  sudo cp -a $DS/root/{Shell,.bashrc,.zshrc} /root/
+else
+  cd $DS
   git pull origin master
   cd -
   # Copy across
-  cp -a $HOME/GitHub/debian-scripts/{Shell,.bashrc,.zshrc} $HOME/
-  sudo cp -a $HOME/GitHub/debian-scripts/root/{Shell,.bashrc,.zshrc} /root/
-elif [[ -d $HOME/GitHub/mine/scripts/debian-scripts ]]; then
-  cd $HOME/GitHub/mine/scripts/debian-scripts
-  git pull origin master
-  cd -
-  # Copy across
-  cp -a $HOME/GitHub/mine/scripts/debian-scripts/{Shell,.bashrc,.zshrc} $HOME/
-  sudo cp -a $HOME/GitHub/mine/scripts/debian-scripts/root/{Shell,.bashrc,.zshrc} /root/
+  cp -a $DS/{Shell,.bashrc,.zshrc} $HOME/
+  sudo cp -a $DS/root/{Shell,.bashrc,.zshrc} /root/
 fi
 
 if ! [[ -d $HOME/.oh-my-zsh ]]; then
   # git clone oh-my-zsh
-  git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
+  git clone https://github.com/robbyrussell/oh-my-zsh $OZ
 else
-  cd $HOME/.oh-my-zsh
+  cd $OZ
   git pull origin master
   cd -
 fi
 
-if ! [[ -d $HOME/GitHub/mine/scripts/zsh-theme ]]; then
+if ! [[ -d $ZT ]]; then
 # Get my self-made zsh-themes
-  git clone https://github.com/fusion809/zsh-theme $HOME/GitHub/mine/scripts/zsh-theme
-  cp -a $HOME/GitHub/mine/scripts/zsh-theme/*.zsh-theme $HOME/.oh-my-zsh/themes/
+  git clone https://github.com/fusion809/zsh-theme $ZT
+  cp -a $ZT/*.zsh-theme $OZ/themes/
 else
   cd $HOME/GitHub/{,mine/}zsh-theme
   git pull origin master
   cd -
-  cp -a $HOME/GitHub/{,mine/}zsh-theme/*.zsh-theme $HOME/.oh-my-zsh/themes/
+  cp -a $HOME/GitHub/{,mine/}zsh-theme/*.zsh-theme $OZ/themes/
 fi
 
-if ! [[ -d $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting ]]; then
+if ! [[ -d $OZ/plugins/zsh-syntax-highlighting ]]; then
   # Get zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
+  git clone $ZSH_URL/zsh-syntax-highlighting $OZ/plugins/zsh-syntax-highlighting
 fi
 
-if ! [[ -d $HOME/.oh-my-zsh/plugins/zsh-history-substring-search ]]; then
-  git clone https://github.com/zsh-users/zsh-history-substring-search $HOME/.oh-my-zsh/plugins/zsh-history-substring-search
+if ! [[ -d $OZ/plugins/zsh-history-substring-search ]]; then
+  git clone $ZSH_URL/zsh-history-substring-search $OZ/plugins/zsh-history-substring-search
 fi
 
 # Change default login shell to Zsh
