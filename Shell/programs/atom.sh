29b54ef680e90349a2bd0c15e822053d09b7b3e3
fusion809 <brentonhorne77@gmail.com>
2016-12-25 20:14:29 +1000

    Updating scripts

--- Shell/programs/atom.sh
+++ Shell/programs/atom.sh
@@ -1,3 +1,4 @@
+#!/bin/zsh
 if [[ -f /usr/bin/atom ]]; then
   ATOM_INSTALLED_VERSION=$(apt-cache show atom | grep "Version:" | cut -d ':' -f 2 | cut -d ' ' -f 2)
 else
@@ -5,7 +6,9 @@ else
 fi
 ATOM_LATEST_VERSION=$(wget -q "https://api.github.com/repos/atom/atom/releases/latest"  -O - | grep -E "https.*atom-amd64.tar.gz" | cut -d '"' -f 4 | cut -d '/' -f 8 | sed 's/v//g')
 
-if [[ $ATOM_INSTALLED_VERSION < $ATOM_LATEST_VERSION ]]; then
+autoload is-at-least
+
+if ! `is-at-least $ATOM_LATEST_VERSION ${ATOM_INSTALLED_VERSION:-0}`; then
   wget -c https://github.com/atom/atom/releases/download/v${ATOM_LATEST_VERSION}/atom-amd64.deb -O /tmp/atom-${ATOM_LATEST_VERSION}-amd64.deb
   sudo dpkg -i /tmp/atom-${ATOM_LATEST_VERSION}-amd64.deb
 fi
--- Shell/programs/vscode.sh
+++ Shell/programs/vscode.sh
@@ -1,3 +1,4 @@
+#!/bin/zsh
 if [[ -f /usr/bin/code ]]; then
   VSCODE_INSTALLED_VERSION=$(apt-cache show code | grep "Version:" | cut -d ':' -f 2 | cut -d ' ' -f 2)
 else
@@ -6,7 +7,9 @@ fi
 VSCODE_URL=$(wget -cq http://code.visualstudio.com/updates -O - | grep "deb-x64" | cut -d '"' -f 8)
 VSCODE_LATEST_VERSION=$(echo $VSCODE_URL | cut -d '/' -f 4)
 
-if [[ $VSCODE_INSTALLED_VERSION < $VSCODE_LATEST_VERSION ]]; then
+autoload is-at-least
+
+if ! `is-at-least ${VSCODE_LATEST_VERSION} ${VSCODE_INSTALLED_VERSION:-0}`; then
   wget -c $VSCODE_URL -O /tmp/code-${VSCODE_LATEST_VERSION}-amd64.deb
   sudo dpkg -i /tmp/code-${VSCODE_LATEST_VERSION}-amd64.deb
 fi
