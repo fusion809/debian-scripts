78b02150a9ff21aacef641eab5545f740b2aea07
fusion809 <brentonhorne77@gmail.com>
2017-03-02 20:35:23 +1000

    Updating scripts

---
+++ Shell/edit/gvim/dotfiles.sh
@@ -0,0 +1,11 @@
+function gvbash {
+	gvim $HOME/.bashrc
+}
+
+function gvrc {
+	gvim $HOME/.vimrc
+}
+
+function gvzsh {
+	gvim $HOME/.zshrc
+}
---
+++ Shell/edit/gvim/github.sh
@@ -0,0 +1,34 @@
+# GitHub/mine/editors/vim
+function vvi {
+	pushd $EDT/vim
+	vim
+	popd
+}
+
+# GitHub/mine/packaging/AppImages
+function vaim {
+	pushd $PKG/AppImages
+	vim
+	popd
+}
+
+# GitHub/mine/packaging/GVim.AppImage
+function vgai {
+	pushd $PKG/GVim.AppImage
+	vim
+	popd
+}
+
+# GitHub/mine/scripts/opensuse-scripts
+function vos {
+	pushd $OSC
+	vim
+	popd
+}
+
+# GitHub/mine/websites/fusion809.github.io
+function vfgi {
+        pushd $FGI
+        vim
+        popd
+}
