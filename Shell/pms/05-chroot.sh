0c82fb343c427ff20a3bd7442e1fb088c8bb0293
fusion809 <brentonhorne77@gmail.com>
2017-03-03 20:45:55 +1000

    Updating scripts

--- Shell/pms/05-chroot.sh
+++ Shell/pms/05-chroot.sh
@@ -1,11 +1,11 @@
 function pacaup {
-  sudo chroot /mnt /usr/bin/pacaur-ubuntu
+	sudo chroot /mnt /usr/bin/pacaur-ubuntu
 }
 
 function archroot {
-  sudo mount /dev/sdb1 /mnt && sudo arch-chroot /mnt /usr/bin/su-fusion809
+	sudo mount /dev/sdb1 /mnt && sudo arch-chroot /mnt /usr/bin/su-fusion809
 }
 
 function archup {
-  sudo chroot /mnt /usr/bin/update-ubuntu
+	sudo chroot /mnt /usr/bin/update-ubuntu
 }
