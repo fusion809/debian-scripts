4447ff18237c5c21d17cf272f72da77fb3da3be8
fusion809 <brentonhorne77@gmail.com>
2017-03-05 09:08:39 +1000

    Updating scripts

--- Shell/13-osc.sh
+++ Shell/13-osc.sh
@@ -28,7 +28,9 @@ function ovimup {
                 if [[ "$1" == "vim" ]]; then
                         sed -i -e "s|$vim_patchversion|$patchversion|g" {$HOME/OBS/home:fusion809/gvim-gtk2,$HOME/AUR/gvim-gtk2,$PKG/PKGBUILDs/gvim-gtk2,$PKG/PKGBUILDs/gvim-gtk3}/PKGBUILD
 
-                        sed -i -e "s|$vim_baseversion.$vim_patchversion|$pkgver|g" $HOME/OBS/home:fusion809/vim-debian/{debian.changelog,debian.dsc,_service}
+                        sed -i -e "s|$vim_baseversion.$vim_patchversion|$pkgver|g" $HOME/OBS/home:fusion809/vim-debian/{debian.dsc,_service}
+
+			sed -i "1s/^/vim (2:$pkgver-1) trusty; urgency=medium\n\n  * New upstream release\n\n -- Brenton Horne <brentonhorne77@gmail.com>  %a, %d %b %Y %H:%M:%S +1000\n\n/" $HOME/OBS/home:fusion809/vim-debian/debian.changelog
 
                         cd $HOME/AUR/gvim-gtk2
                         push "Bumping to $pkgver"
