400f708c7b1340eb07b935fae1060d62354214cb
Brenton Horne <brentonhorne77@gmail.com>
2017-03-06 07:13:35 +1000

    Updating

--- Shell/12-mounts.sh
+++ Shell/12-mounts.sh
@@ -1,14 +1,14 @@
-if ! `grep -qs "/mnt/proc" /proc/mounts`; then
-	sudo mount -t proc proc /mnt/proc
-	sudo mount --rbind /dev /mnt/dev
-	sudo mount --rbind /sys /mnt/sys
-	sudo mount --rbind /tmp /mnt/tmp
-	sudo mount --make-rslave /dev /mnt/dev
-	sudo mount --make-rslave /sys /mnt/sys
-	sudo mount --make-rslave /tmp /mnt/tmp
-	sudo cp -L /etc/resolv.conf /mnt/etc
-fi
+#if ! `grep -qs "/mnt/proc" /proc/mounts`; then
+#	sudo mount -t proc proc /mnt/proc
+#	sudo mount --rbind /dev /mnt/dev
+#	sudo mount --rbind /sys /mnt/sys
+#	sudo mount --rbind /tmp /mnt/tmp
+#	sudo mount --make-rslave /dev /mnt/dev
+#	sudo mount --make-rslave /sys /mnt/sys
+#	sudo mount --make-rslave /tmp /mnt/tmp
+#	sudo cp -L /etc/resolv.conf /mnt/etc
+#fi
 
 function chot {
 	sudo chroot /mnt /bin/bash
-}
+
