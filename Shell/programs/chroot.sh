64e52ac448c50c5d0fafb6c6772e5ad4963c595e
fusion809 <brentonhorne77@gmail.com>
2018-01-08 03:47:09 +1000

    Adding genroot func

--- Shell/programs/chroot.sh
+++ Shell/programs/chroot.sh
@@ -1,6 +1,8 @@
 function genroot {
     if [[ -d $1/root/dev ]]; then
          root="$1/root"
+    elif [[ -d $1/@/dev ]]; then
+         root="$1/@"
     else
          root="$1"
     fi
@@ -18,9 +20,29 @@ function genroot {
     if [[ -f $root/usr/local/bin/su-fusion809 ]]; then
          sudo chroot "$root" /usr/local/bin/su-fusion809
     elif [[ -f $root/bin/zsh ]]; then
-         sudo chroot "$root" /bin/zsh
-    else  
-         sudo chroot "$root" /bin/bash
+         sudo chroot "$root" /bin/env -i \
+               HOME="/root"              \
+               TERM="$TERM"              \
+               PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin \
+               /bin/zsh --login +h
+    elif [[ -f $root/bin/bash ]]; then
+         sudo chroot "$root" /bin/env -i \
+               HOME="/root"              \
+               TERM="$TERM"              \
+               PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;31m\]\$\[\e[m\] \['            \
+               PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin \
+               /bin/bash --login +h
+    elif [[ -f $root/bin/sh ]] && [[ -f $root/bin/env ]]; then
+         sudo chroot "$root" /bin/env -i \
+               HOME="/root"              \
+               TERM="$TERM"              \
+               PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;31m\]\$\[\e[m\] \['            \
+               PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin \
+               /bin/sh --login +h
+    elif [[ -f $root/bin/sh ]] || [[ -L $root/bin/sh ]]; then
+         sudo chroot "$root" /bin/sh
+    else
+         printf "I'm missing a shell, mate!"
     fi
 
     if [[ -f $root/usr/bin/dnf ]]; then
