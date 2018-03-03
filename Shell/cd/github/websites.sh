221fdebf80092b56ff874e590da325c08a9712d1
fusion809 <brentonhorne77@gmail.com>
2018-03-03 22:27:09 +1000

    Updating cdim

--- Shell/cd/github/websites.sh
+++ Shell/cd/github/websites.sh
@@ -1,8 +1,12 @@
 # Websites
 function cdw {
-	cdgm "websites/$1"
+    cdgm "websites/$1"
 }
 
 function cdfgi {
-	cdw "fusion809.github.io/$1"
+    cdw "fusion809.github.io/$1"
+}
+
+function cdim [
+    cdw "images/$1"
 }
